ad60ab9f8c9c5e1eb938f591bcde9917956ae9c6
navi123kumar <naveen.kumar_mca18@gla.ac.in>
2019-01-24 11:10:21 +0530

    my new calculator

--- app/src/main/java/naveenk/com/MainActivity.java
+++ app/src/main/java/naveenk/com/MainActivity.java
@@ -1,13 +1,54 @@
-package naveenk.com;
+package naveenk.com.mycalulator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import naveenk.com.R;
+
+public class MainActivity extends AppCompatActivity
+{
+    EditText etOne, etTwo;
+    Button btnCalc;
+    TextView result;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        etOne = findViewById(R.id.et_1);
+        etTwo = findViewById(R.id.et_2);
+
+        btnCalc = findViewById(R.id.btn_add);
+
+        result = findViewById(R.id.textView);
+
+//        result.setText(null);
+
+        result.setVisibility(View.INVISIBLE);
+
+        btnCalc.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+//                String s = etOne.getText().toString();
+                int a = Integer.parseInt(etOne.getText().toString().trim());
+                int b = Integer.parseInt(etTwo.getText().toString().trim());
+                /*Toast.makeText(MainActivity.this, a+b + "",
+                        Toast.LENGTH_SHORT).show();*/
+                result.setVisibility(View.VISIBLE);
+                result.setText(a+b+"");
+
+            }
+        });
+
     }
 }
